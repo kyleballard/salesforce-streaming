c5a5a851c1caca041c6f9212a368d13e881fb14d
Kyle Ballard <kyle@ballardsoftware.com>
2019-01-11 07:05:28 -0600

    Add project files.

---
+++ Salesforce.NetCore.Streaming.CLI/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Salesforce.NetCore.Streaming.CLI
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
